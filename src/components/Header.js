6154a4d9a62d2ba1f36872b7cddb044977611926
sooraja <sooraja@qburst.com>
2022-04-05 00:26:23 +0530

    Tailwind initialization and Theme set

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,35 @@
 import React from "react";
-import { Button, Col, Navbar, NavItem, NavLink, Row } from 'reactstrap'
-
+import { NavLink } from "react-router-dom";
 
 function Header () {
     return (
-        <Navbar style={{backgroundColor:"rgba(0, 130, 146, 0.7)", justifyContent: "flex-end" }}>
-            <Col style={{flex: 5}}>
-                <h1 style={{color:"white"}}>
-                LOGO
-                </h1>
-            </Col>
-            <Col style={{display: "flex"}}>
-                <NavLink style={{color: "white"}}>Home</NavLink>
-                <NavLink style={{color: "white"}}>About</NavLink>
-                <NavLink style={{color: "white"}}>Sign In</NavLink> 
-            </Col>
-                
-        </Navbar>
+        <div className="header-cont flex-cont">
+            <div className="logo-cont">
+                <a className="logo" href="/">LOGO</a>
+            </div>
+            <nav className="flex flex-grow justify-end">
+                <ul className="flex-cont nav-links">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Sign In</a></li>
+                </ul>
+            </nav>
+        </div>
         
     )
 }
-export default Header
+export default Header;
+
+// <Navbar style={{backgroundColor:"rgba(0, 130, 146, 0.7)", justifyContent: "flex-end" }}>
+// <Col style={{flex: 5}}>
+//     <h1 style={{color:"white"}}>
+//     LOGO
+//     </h1>
+// </Col>
+// <Col style={{display: "flex"}}>
+//     <NavLink style={{color: "white"}}>Home</NavLink>
+//     <NavLink style={{color: "white"}}>About</NavLink>
+//     <NavLink style={{color: "white"}}>Sign In</NavLink> 
+// </Col>
+    
+// </Navbar>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    colors: {
+      black: "#000",
+      blue: "rgba(0,130,146, 0.7)",
+      white: "#fff"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
