aa149d5cae9e2ef8a190cfbad404e359f41a7e82
sooraja <sooraja@qburst.com>
2022-04-12 11:37:27 +0530

    Tool detail css edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     colors: {
       black: "#000",
       blue: "rgba(0,130,146, 0.7)",
+      darkBlue: "#1976D2",
       white: "#fff",
       grey: '#666e68',
       darkGrey: '#555c57'
